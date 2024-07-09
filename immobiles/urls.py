52ab35e30b5226fa043c1d6c27a10622b0d4cf32
pedroiegler <pedroiegler1601@gmail.com>
2024-07-09 15:35:59 -0300

    fix: removing images

--- immobiles/urls.py
+++ immobiles/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('update-customer/<int:pk>/', update_customer, name="update-customer"),
 
     path('create-immobile/', create_immobile, name='create-immobile'),  
-    path('list-immobile/', list_immobile, name="list-immobile"),
+    path('', list_immobile, name="list-immobile"),
     path('update-immobile/<int:pk>/', update_immobile, name="update-immobile"),
     
     path('create-register/<int:id>/', create_register, name='create-register'), 
